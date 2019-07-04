1d9a212f2d5e1e18ca77462072bf033138cc17bc
Colin Simmons <colin.simmons@engineerbetter.com>
2019-07-04 01:11:45 +0100

    kubectl does not have a --version flag
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- install_binaries.sh
+++ install_binaries.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eux
 
-names=( terraform cf jq om fly bosh bbl yq credhub certstrap kubectl shellcheck)
+names=( terraform cf jq om fly bosh bbl yq credhub certstrap shellcheck)
 for name in "${names[@]}"
 do
   chmod +x /usr/bin/$name
   sync # docker bug requires this
   $name --version
 done
+
+chmod +x /usr/bin/kubectl
+sync
+command -v kubectl
